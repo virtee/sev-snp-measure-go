7471a27c905d8f2cd6b795d5200ce031d80592ff
Otto Bittner <cobittner@posteo.net>
2023-08-22 08:39:11 +0200

    Fix tests
    * Add build tag to e2e test so it is not run with unittests.
    * Use OVMFHash from metadataWrapper
    in LaunchDigestFromMetadataWrapper

--- guest/guest_test.go
+++ guest/guest_test.go
@@ -31,15 +31,15 @@ func TestLaunchDigestFromOVMF(t *testing.T) {
 	}{
 		"success bin 1": {
 			ovmfPath: "testdata/ovmf_img_1.bin",
-			// ./sev-snp-measure.py --mode snp:ovmf-hash --ovmf ovmf_img_1.fd
+			// Created by running: ./sev-snp-measure.py --mode snp:ovmf-hash --ovmf ovmf_img_1.fd
 			ovmfHash:  "a58211791a556a630a4319dc9e2ea96cc0e9784dd9f20a4fadf81b26c98d163fcdcb6703884bbbb80d7b1de45b3d84d0",
 			vcpuCount: 2,
-			// ./sev-snp-measure.py --mode snp --vcpus 2 --ovmf ovmf_img_1.fd --vmm-type ec2 --snp-ovmf-hash a58211791a556a630a4319dc9e2ea96cc0e9784dd9f20a4fadf81b26c98d163fcdcb6703884bbbb80d7b1de45b3d84d0
+			// Created by running: ./sev-snp-measure.py --mode snp --vcpus 2 --ovmf ovmf_img_1.fd --vmm-type ec2 --snp-ovmf-hash a58211791a556a630a4319dc9e2ea96cc0e9784dd9f20a4fadf81b26c98d163fcdcb6703884bbbb80d7b1de45b3d84d0
 			expectedHash: "4c6e33087d08fa770259484dddbef367086a15c3e2cf10038dc97229d39c942671c2e22b300178f8de594a3f2fa59303",
 		},
 		"success bin 2": {
 			ovmfPath: "testdata/ovmf_img_2.bin",
-			// ./sev-snp-measure.py --mode snp:ovmf-hash --ovmf ovmf_img_2.fd
+			// Created by running: ./sev-snp-measure.py --mode snp:ovmf-hash --ovmf ovmf_img_2.fd
 			ovmfHash:     "2027a27bb9f7acfd280e4c7bd68a73973b94bf0756e5b282e004b9395f597b8d0eb4defa7d8f6549375aa4d2b146f0f3",
 			vcpuCount:    2,
 			expectedHash: "c2c84b9364fc9f0f54b04534768c860c6e0e386ad98b96e8b98eca46ac8971d05c531ba48373f054c880cfd1f4a0a84e",
@@ -72,23 +72,20 @@ func TestLaunchDigestFromOVMF(t *testing.T) {
 
 func TestLaunchDigestFromMetadataWrapper(t *testing.T) {
 	testCases := map[string]struct {
-		apiObjectPath string
-		ovmfHash      string
-		vcpuCount     int
-		expectedHash  string
-		wantErr       bool
+		apiObjectPath  string
+		vcpuCount      int
+		expectedDigest string
+		wantErr        bool
 	}{
 		"success bin 1": {
-			apiObjectPath: "testdata/ovmf_img_1.json",
-			ovmfHash:      "a58211791a556a630a4319dc9e2ea96cc0e9784dd9f20a4fadf81b26c98d163fcdcb6703884bbbb80d7b1de45b3d84d0",
-			vcpuCount:     2,
-			expectedHash:  "4c6e33087d08fa770259484dddbef367086a15c3e2cf10038dc97229d39c942671c2e22b300178f8de594a3f2fa59303",
+			apiObjectPath:  "testdata/ovmf_img_1.json",
+			vcpuCount:      2,
+			expectedDigest: "4c6e33087d08fa770259484dddbef367086a15c3e2cf10038dc97229d39c942671c2e22b300178f8de594a3f2fa59303",
 		},
 		"success bin 2": {
-			apiObjectPath: "testdata/ovmf_img_2.json",
-			ovmfHash:      "2027a27bb9f7acfd280e4c7bd68a73973b94bf0756e5b282e004b9395f597b8d0eb4defa7d8f6549375aa4d2b146f0f3",
-			vcpuCount:     2,
-			expectedHash:  "c2c84b9364fc9f0f54b04534768c860c6e0e386ad98b96e8b98eca46ac8971d05c531ba48373f054c880cfd1f4a0a84e",
+			apiObjectPath:  "testdata/ovmf_img_2.json",
+			vcpuCount:      2,
+			expectedDigest: "c2c84b9364fc9f0f54b04534768c860c6e0e386ad98b96e8b98eca46ac8971d05c531ba48373f054c880cfd1f4a0a84e",
 		},
 	}
 
@@ -97,9 +94,6 @@ func TestLaunchDigestFromMetadataWrapper(t *testing.T) {
 			assert := assert.New(t)
 			require := require.New(t)
 
-			hash, err := hex.DecodeString(tc.ovmfHash)
-			require.NoError(err)
-
 			data, err := os.ReadFile(tc.apiObjectPath)
 			require.NoError(err)
 
@@ -107,12 +101,12 @@ func TestLaunchDigestFromMetadataWrapper(t *testing.T) {
 			err = json.Unmarshal(data, &apiObject)
 			require.NoError(err)
 
-			launchDigest, err := LaunchDigestFromMetadataWrapper(apiObject, tc.vcpuCount, hash)
+			launchDigest, err := LaunchDigestFromMetadataWrapper(apiObject, tc.vcpuCount)
 			if tc.wantErr {
 				assert.Error(err)
 			} else {
 				assert.NoError(err)
-				expectedHash, err := hex.DecodeString(tc.expectedHash)
+				expectedHash, err := hex.DecodeString(tc.expectedDigest)
 				require.NoError(err)
 				assert.True(bytes.Equal(launchDigest, expectedHash), fmt.Sprintf("expected hash %x, got %x", expectedHash, launchDigest))
 			}
